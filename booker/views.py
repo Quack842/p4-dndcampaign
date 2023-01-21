82d240ae4af36626d5bbdbaf78cd8632ea721917
Nicolene Marais <nici.marais.ie@gmail.com>
2023-01-21 16:35:00 +0000

    linked index field to url.py file and views

--- booker/views.py
+++ booker/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic, View
 
 # Create your views here.
+
+
+class Home(generic.TemplateView):
+    """ This will be the Home/Landing Page """
+    template_name = "index.html"
