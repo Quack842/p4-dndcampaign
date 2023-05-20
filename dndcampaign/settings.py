e89bb56b11253f0a89deec3a39cc3d28e7c2fc9d
Nicolene Marais <nici.marais.ie@gmail.com>
2023-05-20 01:52:44 +0000

    Changed debug to false

--- dndcampaign/settings.py
+++ dndcampaign/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["p4-dndcampaign.herokuapp.com", "localhost", "8000-quack842-p4dndcampaign-06nevxat71m.ws-eu97.gitpod.io"]
 
